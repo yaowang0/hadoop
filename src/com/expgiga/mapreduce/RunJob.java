429ed96eafccb864c54830022fe97dfb0acfb13f
yaowang0 <wyaocn@foxmail.com>
2017-08-27 23:07:59 +0800

    mapreduce

--- src/com/expgiga/mapreduce/RunJob.java
+++ src/com/expgiga/mapreduce/RunJob.java
@@ -11,37 +11,40 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class RunJob {
 
-	public static void main(String[] args) {
-		Configuration config =new Configuration();
-		
-		try {
-			FileSystem fs =FileSystem.get(config);
-			
-			Job job =Job.getInstance(config); //会将src或者classPath路径下的配置文件加载
-			job.setJarByClass(RunJob.class);
-			
-			job.setJobName("wc");
-			
-			job.setMapperClass(WordCountMapper.class);
-			job.setReducerClass(WordCountReducer.class);
-			
-			job.setMapOutputKeyClass(Text.class);
-			job.setMapOutputValueClass(IntWritable.class);
-			
-			FileInputFormat.addInputPath(job, new Path("wc.txt"));
-			
-			Path outpath =new Path("E:/hadoop/output");
-			if(fs.exists(outpath)){
-				fs.delete(outpath, true);
-			}
-			FileOutputFormat.setOutputPath(job, outpath);
-			
-			boolean f= job.waitForCompletion(true);
-			if(f){
-				System.out.println("Doing the Job Task!");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        Configuration config = new Configuration();
+//        config.set("fs.defaultFS", "hdfs://node1:8020");
+//        config.set("yarn.resourcemanager.hostname", "node1");
+//        config.set("mapred.jar", "C:\\Users\\Administrator\\Desktop\\wc.jar");
+
+        try {
+            FileSystem fs = FileSystem.get(config);
+
+            Job job = Job.getInstance(config); //会将src或者classPath路径下的配置文件加载
+            job.setJarByClass(RunJob.class);
+
+            job.setJobName("wc");
+
+            job.setMapperClass(WordCountMapper.class);
+            job.setReducerClass(WordCountReducer.class);
+
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(IntWritable.class);
+
+            FileInputFormat.addInputPath(job, new Path("wc.txt"));
+
+            Path outpath = new Path("E:/hadoop/output");
+            if (fs.exists(outpath)) {
+                fs.delete(outpath, true);
+            }
+            FileOutputFormat.setOutputPath(job, outpath);
+
+            boolean f = job.waitForCompletion(true);
+            if (f) {
+                System.out.println("Doing the Job Task!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
